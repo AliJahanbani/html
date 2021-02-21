d9300913bbfd6862045b272dbf5ea498a2502691
AliJahanbani <79413127+AliJahanbani@users.noreply.github.com>
2021-02-21 19:46:31 +0330

    Add files via upload
    فایل پروژه درس مبتنی بر وب

---
+++ Setting.php
@@ -0,0 +1,12 @@
+<?php
+if ( ! defined('DBHOST'))
+define( 'DBHOST', 'sql203.b6b.ir');
+define( 'DBUSER', 'b6bi_27971515');
+define( 'DBPASS', 'jahanalpha123@');
+define( 'DBNAME', 'b6bi_27971515_database');
+
+$dbHost = DBHOST;
+$dbUser	= DBUSER;		
+$dbPass	= DBPASS;
+$dbname	= DBNAME;
+?>
